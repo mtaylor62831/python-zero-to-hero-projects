a41b4516cf6d1505c8d95748348d4c872bc3704a
Michelle <48816865+mtaylor62831@users.noreply.github.com>
2021-04-08 08:03:12 -0700

    Updated functionality

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,21 +1,3 @@
-def initialize():
-    '''
-    Sets or resets the tic-tac-toe game back to the beginning.
-    The first player will always be X and the second is O
-    '''
-    global gameOver 
-    gameOver = False
-    global gameBoard 
-    gameBoard = [[" "," "," "], [" "," "," "], [" "," "," "]]
-    global player
-    player = "Player 1"
-    global symbol 
-    symbol = "X"
-    print("Let's play tic-tac-toe!")
-    print("When it's your turn you will be prompted for a row and column to play in.")
-    print("The game keeps track of who is X or O")
-    print("Player 1 is first.")
-
 def printBoard():
     for row in gameBoard:
         print(row)
@@ -26,19 +8,71 @@ def positionAllowed(inputPhrase):
     while val not in ["1", "2", "3"]:
         print("That is not a legal value. Try again")
         val = input(inputPhrase)
-    return int(val)
+    return (int(val) - 1) #subtract 1 to get the right index
 
-initialize()
+def legalPlay(row, col):
+    """
+    Returns true if the player has selected an unoccipied space.
+    REturns false if they need to play again
+    """
+    return gameBoard[row][col] == " "
 
-row = positionAllowed(F"{player} what row would you like to play in? Pick a value 1,2 or 3") - 1
-col = positionAllowed(F"{player} which column would you like to play in? Pick a value 1,2 or 3") - 1
-#check to make sure that space is blank
-if gameBoard[row][col] == " ":
-    gameBoard[row][col] = symbol
-else:
-    print("Sorry, someone has already played in that spot. Pick a new one")
-printBoard()
+def playGame():
+    '''
+    This runs the functionality of the tic tac toe game.
+    '''
+    gameOver = False
+    global gameBoard 
+    gameBoard = [[" "," "," "], [" "," "," "], [" "," "," "]]
+    player = "Player 1"
+    symbol = "X"
+    turn = 0
+    print("Let's play tic-tac-toe!")
+    print("When it's your turn you will be prompted for a row and column to play in.")
+    print("The game keeps track of who is X or O")
+    print("Player 1 is first.")
+    while not gameOver:
+        row = positionAllowed(F"{player} what row would you like to play in? Pick a value 1,2 or 3")
+        col = positionAllowed(F"{player} which column would you like to play in? Pick a value 1,2 or 3")
+
+        while not legalPlay(row, col):
+            print("Sorry, someone has already played in that spot. Pick a new one")
+            row = positionAllowed(F"{player} what row would you like to play in? Pick a value 1,2 or 3")
+            col = positionAllowed(F"{player} which column would you like to play in? Pick a value 1,2 or 3")
+
+        #now we finally have a legal value! update the board and display it
+        gameBoard[row][col] = symbol
+        printBoard()
+        #check if there is a winner
+        currentCol = gameBoard[0][col] + gameBoard[1][col] + gameBoard[2][col]
+        currentRow = gameBoard[row][0] + gameBoard[row][1] + gameBoard[row][2]
+        rightDiagonal = gameBoard[2][0] + gameBoard[1][1] + gameBoard[0][2]
+        leftDiagonal = gameBoard[0][0] + gameBoard [1][1] + gameBoard[2][2]
+        winVal = symbol*3
+        turn += 1
+        #still need to add case for the diagonal
+        if winVal in [currentCol, currentRow, rightDiagonal, leftDiagonal]:
+            gameOver = True
+            print(F"WOO HOO {player} has won the game!")
+            #add something to ask if we want to play again
+        elif turn == 9:
+            #if we are on turn 9 with no winner, the game is over.
+            gameOver = True
+            print("Both players made smart play. No winner is possible. Game over.")
+        #If there is no winner, swap the active player so we can take another turn    
+        else:
+            if player == "Player 1":
+                player = "Player 2"
+                symbol = "O"
+            else:
+                player = "Player 1"
+                symbol = "X"
+    #check if the player wants to go again
+    playAgain = input("Do you want to play again? Type y for Yes, type n for No")
+    if playAgain in ["y", "Y"]:
+        playGame()
+    else:
+        print("Thanks for playing! Enjoy the rest of your day")
 
 
-
-
+playGame()
